30cf989390de1e4a73b268cb675990c9e7adbe28
anas alhmoud <a.alhmoud@outlook.com>
2017-09-18 16:56:51 +0300

    service worker

---
+++ workbox-cli-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "globDirectory": "dist\\",
+  "globPatterns": [
+    "**/*.{js,ico,css,woff2,png,html,json}"
+  ],
+  "swDest": "dist/sw.js",
+  "globIgnores": [
+    "..\\workbox-cli-config.js"
+  ]
+};
